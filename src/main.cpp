7bc67d7fc3397d230f8560318dbddf0fbc7efbe8
Muhammad Saad <msaadahmed039@gmail.com>
2021-12-30 20:30:36 +0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+
+int main(int argc, char const *argv[]){
+
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* windows = glfwCreateWindow(800, 600, "Hello OpenGL", NULL, NULL);
+
+    if (!windows) {
+        std::cout << "Failed to create the window " << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(windows);
+
+    while (!glfwWindowShouldClose(windows)) { glfwPollEvents(); }
+
+    glfwDestroyWindow(windows);
+    glfwTerminate();
+
+    return EXIT_SUCCESS;
+}
