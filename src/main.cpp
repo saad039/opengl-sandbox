32882d5ad1e62966e7fb259d5eeb417f6081a77f
Muhammad Saad <msaadahmed039@gmail.com>
2022-01-27 15:48:46 +0500

    adds skeleton for macOS dev

--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,66 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+constexpr static int WINDOW_WIDTH = 4000;
+constexpr static int WINDOW_HEIGHT = 4000;
 
-int main(int argc, char const *argv[]){
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+
+int main(int argc, char const* argv[]) {
 
     glfwInit();
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
 
-    GLFWwindow* windows = glfwCreateWindow(800, 600, "Hello OpenGL", NULL, NULL);
+#if defined __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
-    if (!windows) {
-        std::cout << "Failed to create the window " << std::endl;
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Hello OpenGL", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to create the window " << std::endl;
         glfwTerminate();
         return -1;
     }
 
-    glfwMakeContextCurrent(windows);
+    glfwMakeContextCurrent(window);
 
-    while (!glfwWindowShouldClose(windows)) { glfwPollEvents(); }
+    // Loading openGL's function pointers into memory
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize glad" << std::endl;
+        return -1;
+    }
 
-    glfwDestroyWindow(windows);
+    // Informating openGL about the dimentions of our window.
+    // The first two parameters set the location of the lower left corner of the window.
+    // Behind the scenes OpenGL uses the data specified via glViewport to transform the 2D
+    // coordinates it processed to coordinates on your screen. For example, a processed point of
+    // location (-0.5,0.5) would (as its final transformation) be mapped to (200,450) in screen
+    // coordinates. Note that processed coordinates in OpenGL are between -1 and 1 so we effectively
+    // map from the range (-1 to 1) to (0, 800) and (0, 600).
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    //informing openGL about window being resized
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+ 
+    //Render loop
+    while (!glfwWindowShouldClose(window)) { 
+        glfwSwapBuffers(window);
+        glfwPollEvents(); 
+    }
+
+    glfwDestroyWindow(window);
     glfwTerminate();
 
     int value = 25ul;
     return EXIT_SUCCESS;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    std::clog << "Window resized to ("<<width <<','<<height<<")\n";
+    glViewport(0, 0, width, height);
 }
